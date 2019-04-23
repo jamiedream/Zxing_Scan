5c244a1f0d90d5cc23702a538339a321d52a1048
Chunying Yen <chunyingyen.tw@gmail.com>
2019-04-23 12:01:21 +0800

    generate Wifi config code, scan Wifi config and connect

--- app/src/main/java/com/followme/scanapplication/MainActivity.java
+++ app/src/main/java/com/followme/scanapplication/MainActivity.java
@@ -1,23 +1,42 @@
 package com.followme.scanapplication;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.followme.zxing_android_integration.IntentIntegrator;
 
+import java.lang.ref.WeakReference;
+
+
+/**
+ * Warning: Due to security and performance limitations, use of WEP networks is discouraged.
+ *
+ * */
 
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = MainActivity.class.getSimpleName();
     private final int SCAN_CODE = 30;
+    private final int WIFI_SCAN_CODE = 31;
+    private TextView scannedText;
+    private TextView scannedSSID;
+    private QRCodeUtil qrCodeUtil;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -25,45 +44,179 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView click = findViewById(R.id.click);
-        click.setOnTouchListener(new View.OnTouchListener() {
+        qrCodeUtil = new QRCodeUtil();
+
+        scanGetText();
+        generateWifiCode();
+        scanToConnectWifi();
+//        scanToConnectedWifi();
+
+        Button generateConnectedWifiCode = findViewById(R.id.generate_connected_wifi_code);
+        generateConnectedWifiCode.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()){
-                    case MotionEvent.ACTION_DOWN:
-                        Intent intentCheck = new Intent("com.google.zxing.client.android.SCAN");
-                        if(getPackageManager().queryIntentActivities(intentCheck, PackageManager.MATCH_DEFAULT_ONLY).size() == 0) {
-                            //installed scan app if not detected
-                            Log.i(TAG, "未安裝");
-                            IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
-                            integrator.initiateScan();
-                        }else {
-                            //open scan app
-                            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-                            intent.putExtra("SCAN_MODE", "SCAN_MODE");
-                            startActivityForResult(intent, SCAN_CODE);
-                        }
-                        break;
-                    default:
-                        break;
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    private void scanGetText(){
+        Button click = findViewById(R.id.click);
+        scannedText = findViewById(R.id.scanned_text);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCheck = new Intent("com.google.zxing.client.android.SCAN");
+                if(getPackageManager().queryIntentActivities(intentCheck, PackageManager.MATCH_DEFAULT_ONLY).size() == 0) {
+                    //installed scan app if not detected
+                    Log.i(TAG, "未安裝");
+                    IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                    integrator.initiateScan();
+                }else {
+                    //open scan app
+                    Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                    intent.putExtra("SCAN_MODE", "SCAN_MODE");
+                    startActivityForResult(intent, SCAN_CODE);
                 }
-                return false;
+            }
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                switch (event.getAction()){
+//                    case MotionEvent.ACTION_DOWN:
+//                        Intent intentCheck = new Intent("com.google.zxing.client.android.SCAN");
+//                        if(getPackageManager().queryIntentActivities(intentCheck, PackageManager.MATCH_DEFAULT_ONLY).size() == 0) {
+//                            //installed scan app if not detected
+//                            Log.i(TAG, "未安裝");
+//                            IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+//                            integrator.initiateScan();
+//                        }else {
+//                            //open scan app
+//                            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+//                            intent.putExtra("SCAN_MODE", "SCAN_MODE");
+//                            startActivityForResult(intent, SCAN_CODE);
+//                        }
+//                        break;
+//                    default:
+//                        break;
+//                }
+//                return false;
+//            }
+        });
+    }
+
+    private void generateWifiCode(){
+        Button wifiCode = findViewById(R.id.generate_wifi_code);
+        final Spinner spinner = findViewById(R.id.spinner);
+        final String[] types = getResources().getStringArray(R.array.security_type);
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, types);
+        spinner.setAdapter(spinnerAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                findViewById(R.id.pwd_text).setVisibility(types[position].equals(types[0])? View.INVISIBLE: View.VISIBLE );
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+
+            }
+
+        });
+
+        wifiCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText ssid = findViewById(R.id.ssid_text);
+                EditText pwd = findViewById(R.id.pwd_text);
+                String ssidText = ssid.getText().toString();
+                String pwdText = pwd.getText().toString();
+                CheckBox isHideCheckbox = findViewById(R.id.is_hide_ssid);
+                ImageView qrCode = findViewById(R.id.qr_code_preview);
+                String qrString = "";
+                if(ssidText.length() > 0){
+                    switch (spinner.getSelectedItem().toString()) {
+                        case "Nano":
+                            qrString = qrCodeUtil.getConfig("omit", ssidText, "", isHideCheckbox.isChecked());
+                            break;
+                        case "WEP":
+                            if(pwdText.length() > 0) {
+                                qrString = qrCodeUtil.getConfig("WEP", ssidText, pwdText, isHideCheckbox.isChecked());
+                            }else qrString = "";
+                            break;
+                        case "WPA/WPA2":
+                            if(pwdText.length() > 0) {
+                                qrString = qrCodeUtil.getConfig("WPA", ssidText, pwdText, isHideCheckbox.isChecked());
+                            }else qrString = "";
+                            break;
+                        default:
+                            qrString = "";
+                            break;
+                    }
+
+                    qrCode.setImageBitmap(
+                            qrCodeUtil.textToImageEncode(
+                                    new WeakReference<Context>(MainActivity.this),
+                                    qrString,
+                                    getCodeDimension()
+                            )
+                    );
+                }
             }
         });
     }
+
+    private int getCodeDimension(){
+        return getResources().getDisplayMetrics().widthPixels / 2;
+    }
+
+    private void scanToConnectWifi(){
+        scannedSSID = findViewById(R.id.scaned_ssid);
+        Button scanToConnectWifi = findViewById(R.id.scan_to_connect_wifi);
+        scanToConnectWifi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCheck = new Intent("com.google.zxing.client.android.SCAN");
+                if(getPackageManager().queryIntentActivities(intentCheck, PackageManager.MATCH_DEFAULT_ONLY).size() == 0) {
+                    //installed scan app if not detected
+                    Log.i(TAG, "未安裝");
+                    IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                    integrator.initiateScan();
+                }else {
+                    //open scan app
+                    Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                    intent.putExtra("SCAN_MODE", "WIFI_SCAN_MODE");
+                    startActivityForResult(intent, WIFI_SCAN_CODE);
+                }
+            }
+        });
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
 
         //get scan result
-        if(requestCode == SCAN_CODE && resultCode == RESULT_OK){
+        if(resultCode == RESULT_OK){
 
-            String contents = intent.getStringExtra("SCAN_RESULT");
-            String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+            switch (requestCode){
+                case SCAN_CODE:
+                    scannedText.setText(intent.getStringExtra("SCAN_RESULT") + ":" + intent.getStringExtra("SCAN_RESULT_FORMAT"));
+                    break;
+                case WIFI_SCAN_CODE:
+//                    scannedText.setText(intent.getStringExtra("SCAN_RESULT"));
 
-            Log.i(TAG, contents + " : " + format);
+                    String[] params = qrCodeUtil.getWifiConfig(intent.getStringExtra("SCAN_RESULT"));
+                    scannedSSID.setText("Connecting to SSID...... " + params[0]);
+                    WifiManager wifiManager = (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                    qrCodeUtil.connectToWifi(wifiManager, params[0], params[1], params[2], Boolean.parseBoolean(params[3]));
 
+                    break;
+                default:
+                    break;
+            }
         }
     }
+
 }
---
+++ app/src/main/java/com/followme/scanapplication/QRCodeUtil.java
@@ -0,0 +1,202 @@
+package com.followme.scanapplication;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import com.followme.zxing_android_integration.IntentIntegrator;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+import java.lang.ref.WeakReference;
+import java.util.List;
+
+public class QRCodeUtil {
+
+    private String TAG = QRCodeUtil.class.getSimpleName();
+    /**
+     * Decode
+     *
+     * Parameters refer to Intents.java final class SCAN
+     * */
+    private final String ACTION = "com.google.zxing.client.android.SCAN";
+    private final int SCAN_CODE = 30;
+    private final String SCAN_RESULT = "SCAN_RESULT";
+    private final String SCAN_RESULT_FORMAT = "SCAN_RESULT_FORMAT";
+
+    public void intentToInstalledApp(WeakReference<Activity> activity){
+        IntentIntegrator integrator = new IntentIntegrator(activity.get());
+        integrator.initiateScan();
+    }
+
+    public void openApp(WeakReference<Activity> activity){
+        Intent intent = new Intent(ACTION);
+        activity.get().startActivityForResult(intent, SCAN_CODE);
+    }
+
+    public int getScanCode(){
+        return SCAN_CODE;
+    }
+
+    public String getScaneResult(Intent intent){
+        return intent.getStringExtra(SCAN_RESULT);
+    }
+
+    public String getScaneResultFormat(Intent intent){
+        return intent.getStringExtra(SCAN_RESULT_FORMAT);
+    }
+
+    /**
+     * Encode
+     * */
+
+    public Bitmap textToImageEncode(WeakReference<Context> context, String value, int dimen){
+
+        int width = dimen;
+        int height = dimen;
+        BitMatrix bitMatrix = null;
+        try {
+            bitMatrix = new MultiFormatWriter().encode(
+                    value,
+                    BarcodeFormat.QR_CODE,
+                    width,
+                    height);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+
+        int[] pixels = new int[width * height];
+        for(int numY = 0; numY < height; numY ++){
+            int offset = numY * width;
+            for(int numX = 0; numX < width; numX ++){
+                pixels[offset + numX] = ContextCompat.getColor(context.get(), bitMatrix.get(numX, numY)? R.color.black : R.color.white);
+            }
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        return bitmap;
+    }
+
+    /**
+     * Wifi config
+     *
+     * */
+
+    public String getConfig(String type, String ssid, String pwd, boolean isHide){
+
+        if(isHide){
+            return String.format("WIFI:T:%1$s;S:%2$s;P:%3$s;;", type, ssid, pwd);
+        }else{
+            return String.format("WIFI:T:%1$s;S:%2$s;P:%3$s;H:true;;", type, ssid, pwd);
+        }
+
+    }
+
+    /**
+     * Wifi parameters
+     * @param callbackString: 0: ssid, 1:pwd, 2:type, 3:isHide
+     * */
+    public String[] getWifiConfig(String callbackString){
+        String[] parameters = new String[4];
+
+        String[] strings = callbackString.split(";");
+
+//        String ssid = "";
+//        String pwd = "";
+//        String type = "";
+//        boolean isHide = false;
+
+        String SSID = "S:";
+        String PWD = "P:";
+        String TYPE = "T:";
+        String HIDE = "H:";
+
+        for(String s: strings){
+            if(s.contains(SSID)){
+                parameters[0] = subStringParameter(s, SSID);
+//                ssid = subStringParameter(s, SSID);
+            }else if(s.contains(PWD)){
+                parameters[1] = subStringParameter(s, PWD);
+//                pwd = subStringParameter(s, PWD);
+            }else if(s.contains(TYPE)){
+                parameters[2] = subStringParameter(s, TYPE);
+//                type = subStringParameter(s, TYPE);
+            }else if(s.contains(HIDE)){
+                parameters[3] = subStringParameter(s, HIDE);
+//                isHide = Boolean.parseBoolean(subStringParameter(s, HIDE));
+            }else if(!s.contains(HIDE)){
+                parameters[3] = "false";
+
+            }
+        }
+
+        return parameters;
+    }
+
+    private String subStringParameter(String string, String para){
+        int start;
+        int end;
+        start = string.indexOf(para) + 2;
+        end = string.length();
+        return string.substring(start, end);
+    }
+
+    /**
+     * Connect to specific wifi
+     * */
+    public void connectToWifi(WifiManager wifiManager, String ssid, String pwd, String type, boolean isHide){
+
+        Log.i(TAG, ssid + ":" + pwd + ":" + type + ":" + isHide);
+        WifiConfiguration config = new WifiConfiguration();
+        config.SSID = "\"" + ssid + "\"";
+
+        Log.i(TAG, type);
+        switch (type){
+            case "Nano":
+                config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+                break;
+            case "WEP":
+                config.wepKeys[0] = "\"" + pwd + "\"";
+                config.wepTxKeyIndex = 0;
+                config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+                config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+                break;
+            case "WPA":
+            case "WPA2":
+                config.preSharedKey = "\"" + pwd + "\"";
+                config.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+                config.status = WifiConfiguration.Status.ENABLED;
+                break;
+            default:
+                break;
+        }
+
+        config.hiddenSSID = isHide;
+
+        wifiManager.addNetwork(config);
+
+        List<WifiConfiguration> list = wifiManager.getConfiguredNetworks();
+        for( WifiConfiguration i : list ) {
+            if (i.SSID != null && i.SSID.equals("\"" + ssid + "\"")) {
+                Log.i(TAG, i.SSID);
+                if(!wifiManager.isWifiEnabled()){
+                    wifiManager.setWifiEnabled(true);
+                }
+                wifiManager.disconnect();
+                Log.i(TAG, i.networkId + "");
+                wifiManager.enableNetwork(i.networkId, true);
+                wifiManager.reconnect();
+                break;
+            }
+        }
+    }
+
+}
